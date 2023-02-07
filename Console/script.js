d7877346594ca864bcd41ae9f5c8387a0b399414
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-02-07 22:49:37 +0000

    Console

---
+++ Console/script.js
@@ -0,0 +1,31 @@
+/*Hello World
+It is programming tradition that the first thing you do in any 
+language is make it output 'Hello world!'.
+We'll do this in JavaScript, using a command called console.log(). 
+The console.log() method writes a message to the console.
+The console is a tool which is mainly used to log information - it's
+ useful for testing purposes.
+ EXERCISE
+(This exercise will help you understand how to run a basic JS script 
+and explore the different ways you can run JS code)
+Create your first js1-week1.js script
+Type console.log("Hello World!")
+There are 2 ways you can run this script - one way is by pressing F5 
+in your VS Code application. Can you find out what the second way is? 
+Pair up and use a search engine to find out! Choose your favourite method 
+and use that from now on. :::
+BONUS: there is a third way of running JS code (notice how I haven't 
+said scipt) - do you know what that is?
+ */
+console.log("Hello World!")
+/*
+EXERCISE
+(This exercise will help you expand your understanding of console.log)
+
+Write 10 statements like these, but in different languages.
+
+For example:
+Halo, dunia! // Indonesian
+Ciao, mondo! // Italian
+Hola, mundo! // Spanish
+ */
