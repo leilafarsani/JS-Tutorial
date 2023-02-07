d52140221804dc94d3857c9a9a1f3abb84867500
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-02-07 22:58:07 +0000

    Variables

---
+++ Variables/script.js
@@ -0,0 +1,22 @@
+/*Variables
+When you write code, you'll want to create shortcuts to data values 
+so you don't have to write out the same value every time.
+We can use a variable to create a reference to a value. Variables 
+can be thought of as named containers. You can place data into these 
+containers and then refer to the data simply by naming the container.
+Before you use a variable in a JavaScript program, you must declare it. 
+Variables are declared with let and const keywords as follows.*/
+let greeting = "Hello world";
+console.log(greeting);
+const name = "Irina";
+console.log(name);
+/*
+EXERCISE
+Add a variable greeting to js1-week1.js and assign a greeting of your choice 
+to the variable Print your greeting to the console 3 times. You should see your
+greeting 3 times on the console, one on each line.
+ */
+let greetings = "How are you doing?"
+console.log(greetings);
+console.log(greetings);
+console.log(greetings);
