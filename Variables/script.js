d69a8a1030dbecebc3c6aaf27485e82569a70d29
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-02-23 12:46:41 +0000

    Content Added!

--- Variables/script.js
+++ Variables/script.js
@@ -1,11 +1,153 @@
 /*Variables
+Variables are containers for storing data (storing data values).
 When you write code, you'll want to create shortcuts to data values 
 so you don't have to write out the same value every time.
 We can use a variable to create a reference to a value. Variables 
 can be thought of as named containers. You can place data into these 
 containers and then refer to the data simply by naming the container.
 Before you use a variable in a JavaScript program, you must declare it. 
-Variables are declared with let and const keywords as follows.*/
+Variables are declared with let and const keywords as follows.
+
+4 Ways to Declare a JavaScript Variable:
+Using var
+Using let
+Using const
+Using nothing
+
+Always declare JavaScript variables with var,let, or const.
+
+The var keyword is used in all JavaScript code from 1995 to 2015.
+
+The let and const keywords were added to JavaScript in 2015.
+
+If you want your code to run in older browsers, you must use var.
+
+If you want a general rule: always declare variables with const.
+
+If you think the value of the variable can change, use let.
+
+JavaScript Identifiers
+All JavaScript variables must be identified with unique names.
+
+These unique names are called identifiers.
+
+Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+
+The general rules for constructing names for variables (unique identifiers) are:
+
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter.
+Names can also begin with $ and _ (but we will not use it in this tutorial).
+Names are case sensitive (y and Y are different variables).
+Reserved words (like JavaScript keywords) cannot be used as names.
+
+The Assignment Operator
+The assignment (=) operator is used to assign a value to a variable. The assignment operation
+evaluates to the assigned value. Chaining the assignment operator is possible 
+in order to assign a single value to multiple variables.
+In JavaScript, the equal sign (=) is an "assignment" operator,
+not an "equal to" operator.
+
+This is different from algebra. The following does not make sense in algebra:
+x = x + 5
+In JavaScript, however, it makes perfect sense: it assigns the value of x + 5 to x.
+(It calculates the value of x + 5 and puts the result into x. The value of x is incremented by 5.)
+
+let x = 5;
+let y = 10;
+let z = 25;
+
+x = y; // x is 10
+x = y = z; // x, y and z are all 25
+Note
+The "equal to" operator is written like == in JavaScript.
+
+Addition assignment (+=)
+The addition assignment (+=) operator adds the value of the right operand to a variable 
+and assigns the result to the variable. The types of the two operands determine
+the behavior of the addition assignment operator. Addition or concatenation is possible.
+Syntax
+x += y // x = x + y
+
+let baz = true;
+// Boolean + Number -> addition
+baz += 1; // 2
+// Number + Boolean -> addition
+baz += false; // 2
+
+let foo = "foo";
+// String + Boolean -> concatenation
+foo += false; // "foofalse"
+
+// String + String -> concatenation
+foo += "bar"; // "foofalsebar"
+
+let bar = 5;
+// Number + Number -> addition
+bar += 2; // 7
+
+// Number + String -> concatenation
+bar += "foo"; // "7foo"
+
+Multiplication assignment (*=)
+The multiplication assignment (*=) operator multiplies a variable by the value of
+the right operand and assigns the result to the variable.
+let bar = 5;
+
+bar *= 2; // 10
+bar *= "foo"; // NaN
+
+
+Exponentiation assignment (**=)
+The exponentiation assignment (**=) operator raises the value of a variable to the power of
+the right operand.
+let bar = 5;
+
+bar **= 2; // 25
+bar **= "foo"; // NaN
+
+Division assignment (/=)
+The division assignment (/=) operator divides a variable by the value of the right operand.
+let bar = 5;
+syntax
+x /= y // x = x / y
+bar /= 2; // 2
+
+Remainder assignment (%=)
+The remainder assignment (%=) operator divides a variable by the value of 
+the right operand and assigns the remainder to the variable.
+Syntax
+x %= y // x = x % y
+let bar = 5;
+
+bar %= 2; // 1
+bar %= "foo"; // NaN
+bar %= 0; // NaN
+
+Subtraction assignment (-=)
+The subtraction assignment (-=) operator subtracts the value of the right
+operand from a variable and assigns the result to the variable.
+Syntax
+x -= y // x = x - y
+let bar = 5;
+
+bar -= 2; // 3
+bar -= "foo"; // NaN
+
+Logical OR assignment (||=)
+The logical OR assignment (x ||= y) operator only assigns if x is falsy.
+
+Logical AND assignment (&&=)
+The logical AND assignment (x &&= y) operator only assigns if x is truthy.
+
+Left shift assignment (<<=)
+The left shift assignment (<<=) operator moves the specified amount of bits to the
+left and assigns the result to the variable.
+
+Right shift assignment (>>=)
+The right shift assignment (>>=) operator moves the specified amount of bits to the right
+and assigns the result to the variable.
+*/
 let greeting = "Hello world";
 console.log(greeting);
 const name = "Irina";
