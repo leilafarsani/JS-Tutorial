0ff456c9841b1e5b9bf525f3b067b6d8cc15cf15
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-03-26 23:15:59 +0100

    functions with for loop on arrays

--- Practice/script.js
+++ Practice/script.js
@@ -1,8 +1,50 @@
-const color ="blue";
-console.log(color);
-let myName = "Leila"
-const msg = `Hi my name is ${myName} and`
-console.log(msg);
+// const color ="blue";
+// console.log(color);
+// let myName = "Leila"
+// const msg = `Hi my name is ${myName} and`
+// console.log(msg);
 
-console.log(x);
-var x;
+// console.log(x);
+// var x;
+names=["Mar","Amy","Lei","Hen","Sagh","Far"]
+
+ function secondMatchesAmy(array) {
+  if (array.includes("Hen" )) {
+    return "Second element matched!";
+  }
+  return "Second element not matched";
+}
+
+console.log(secondMatchesAmy(names));
+
+// const trainees = ["Ahmed", "Maria", "Atanas", "Nahidul", "Jack"];
+
+// function getName(arr){
+// for(let i=0; i<arr.length; i++){
+// console.log(arr[i]);
+// }
+// }
+// getName(trainees);
+num =[2,5,6,8,100]
+
+// function getSquare(numArray){
+//   let array=[];
+//   for (let i=0; i<numArray.length; i++){
+//  array.push(numArray[i]*numArray[i]);
+// }
+// return array;
+// }
+// //console.log(getSquare(num));
+
+// function getEvenNums(numArray){
+//   let newArr=[];
+//   for (let i=0; i<numArray.length; i++){
+//     if(numArray[i]%2===0){
+//       newArr.push(numArray[i]);
+//     }
+//   }
+//   return getSquare(newArr);
+// }
+
+// console.log(getEvenNums(num));
+
